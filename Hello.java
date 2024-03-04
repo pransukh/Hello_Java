e421e640e5643e8fb08f4f07996938c10e7b3d2b
Pran Sukh <hitman47silent994c4@gmail.com>
2024-03-05 01:18:31 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World, By Pran");
-      System.out.println("Hello, World, New line by pran");
-      System.out.println("Hello, World, New line by balu");
-   }
-}
